8edbf9e39aa313e338c2c6661ff283d3ae02ef5d
Morsadi <badreddine.morsadi@github.com>
2019-08-02 14:48:15 -0700

    Settings

---
+++ build/precache-manifest.776f7bfb39e5143ebd6dce434fdbca12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87b9d8923e3cce503d9070e7dda56956",
+    "url": "/Morsadi/teacher-assess.git/index.html"
+  },
+  {
+    "revision": "1bc767981b59403f0ac8",
+    "url": "/Morsadi/teacher-assess.git/static/css/main.dee0730d.chunk.css"
+  },
+  {
+    "revision": "97cbe95130965e57c2bc",
+    "url": "/Morsadi/teacher-assess.git/static/js/2.1fe651b6.chunk.js"
+  },
+  {
+    "revision": "1bc767981b59403f0ac8",
+    "url": "/Morsadi/teacher-assess.git/static/js/main.0bfa9d92.chunk.js"
+  },
+  {
+    "revision": "8cced90b4a6b6b5364c2",
+    "url": "/Morsadi/teacher-assess.git/static/js/runtime~main.9e635d90.js"
+  }
+]);
