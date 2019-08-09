b62fd51acb2fe55ebb93cf39e20c6194a4b93984
Morsadi <badreddine.morsadi@github.com>
2019-08-08 19:07:46 -0700

    Package Changes

---
+++ build/precache-manifest.0feba0dad8e1e71f1f245261102d4b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446f05749b787bc47645102950ba73e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6a80048e95ea22bb7bd9",
+    "url": "./static/css/main.dee0730d.chunk.css"
+  },
+  {
+    "revision": "aaf540d559acd184b9c1",
+    "url": "./static/js/2.7894bc9c.chunk.js"
+  },
+  {
+    "revision": "6a80048e95ea22bb7bd9",
+    "url": "./static/js/main.a5118f35.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/teacher-assess/precache-manifest.dd3225af8e6938c4684450fe31282dac.js"
+  "./precache-manifest.0feba0dad8e1e71f1f245261102d4b1e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/teacher-assess/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
